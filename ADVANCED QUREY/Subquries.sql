14c7ece1f5b3714c1a5814779042d8fa3c935683
Gamer <138502330+Payasgo@users.noreply.github.com>
2023-09-15 05:32:10 +0530

    Update Subquries.sql

--- ADVANCED QUREY/Subquries.sql
+++ ADVANCED QUREY/Subquries.sql
@@ -32,4 +32,10 @@ FROM (SELECT EmployeeID, Salary, Avg(Salary) over() As AllAvgsalary
 
 
 -- Subqurey in WHERE
+SELECT EmployeeID, jobTitle, Salary
+FROM EmployeeSalary
+WHERE EmployeeID in (
+		SELECT EmployeeID
+		FROM EmployeeDemographics
+		WHERE Age > 30)
 
